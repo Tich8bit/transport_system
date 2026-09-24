35753da2acaaab64869129b616c5776b6e571908
Tich8bit <vkulikov332@gmail.com>
2026-09-24 21:51:22 +0300

    Remove from h to cpp vehicle

--- HeaderFolder/vehicle.h
+++ HeaderFolder/vehicle.h
@@ -23,36 +23,12 @@ public:
     void setRegNumber(std::string_view newRegNumber);
     void setMaxSpeed(int newMaxSpeed);
     void setDriver(std::shared_ptr<Driver> newDriver);
-    friend std::ostream& operator<<(std::ostream& os, const Vehicle& vehicle) {
-        os << "ТС: " << vehicle._model
-        << " | Госномер: " << vehicle._regNumber
-        << " | Год выпуска: " << vehicle._year
-        << " | Вместимость: " << vehicle._capacity << " чел."
-        << " | Скорость: " << vehicle._maxSpeed << " км/ч.";
-        if (vehicle._driver) {
-            os << " | Водитель: " << vehicle._driver->getFullName();
-        } else {
-            os << " | Водитель не назначен\n";
-        }
-        return os;
-    }
-    friend std::istream& operator>>(std::istream& is, Vehicle& vehicle) {
-        vehicle._regNumber = inputString("Введите госномер: ");
-        vehicle._model     = inputString("Введите модель: ");
-        vehicle._year      = inputInt("Введите год выпуска: ");
-        vehicle._capacity  = inputInt("Введите вместимость: ");
-        vehicle._maxSpeed  = inputInt("Введите скорость: ");
-        return is;
-    }
+    friend std::ostream& operator<<(std::ostream& os, const Vehicle& vehicle);
+    friend std::istream& operator>>(std::istream& is, Vehicle& vehicle);
     friend bool isNewer(const Vehicle& a, const Vehicle& b);
-    friend bool operator==(const Vehicle& a, const Vehicle& b) {
-        return a._regNumber == b._regNumber;
-    }
-    friend std::strong_ordering operator<=>(const Vehicle& a, const Vehicle& b) {
-        if (a._maxSpeed < b._maxSpeed) return std::strong_ordering::less;
-        if (a._maxSpeed > b._maxSpeed) return std::strong_ordering::greater;
-        return std::strong_ordering::equal;
-    }
+    friend bool operator==(const Vehicle& a, const Vehicle& b);
+    friend std::strong_ordering operator<=>(const Vehicle& a, const Vehicle& b);
+
 private:
     std::string _regNumber;
     std::string _model;
